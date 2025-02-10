fe2654bc8cc00fc698705ce372152ce547b3af1e
saif ivna alam <alam.saifivna@gmail.com>
2025-02-10 13:47:25 +0600

    initial commit

---
+++ lib/home/home_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:guitartuner/home/pitch_cubit.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final pitchCubitState = context.watch<PitchCubit>().state;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Guitar tuner"),
+      ),
+      body: Center(
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Text(
+                pitchCubitState.note,
+                style: const TextStyle(color: Colors.black87, fontSize: 65.0, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                pitchCubitState.fz,
+                style: const TextStyle(color: Colors.black87, fontSize: 65.0, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                pitchCubitState.expectedFq,
+                style: const TextStyle(color: Colors.black87, fontSize: 65.0, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                pitchCubitState.status,
+                style: const TextStyle(
+                  color: Colors.black87,
+                  fontSize: 18.0,
+                ),
+              ),
+            ]),
+      ),
+    );
+  }
+}
---
+++ lib/home/tunning_state.dart
@@ -0,0 +1,8 @@
+class TunningState {
+  final String note;
+  final String status;
+  final String fz;
+  final String expectedFq;
+
+  TunningState({required this.note, required this.status, required this.fz, required this.expectedFq});
+}
